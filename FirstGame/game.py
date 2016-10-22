df31c973708d2d7fce06a38efb909386d8c2af00
HSLoveZL <hjmrezl@outlook.com>
2016-10-22 10:48:34 +0800

    update game.py

--- FirstGame/game.py
+++ FirstGame/game.py
@@ -1,9 +1,11 @@
 # coding=utf-8
+from random import randint
+number = randint(1, 10)
 print '---------------Hello,this is my first game!---------------'
 temp = input("不妨猜猜我是哪个数字:")
 guess = int(temp)
 
-if guess == 6:
+if guess == number:
     print '我勒个去，这都被你猜中了！'
 else:
     print '不好意思，猜错啦！'
